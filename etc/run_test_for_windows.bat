ff7fb940d0c60bb5043273eace68881b64abf067
Konstantin Kakunin (lanit) <kakunin@lanit.ru>
2020-03-05 09:42:10 +0300

    проба

--- etc/run_test_for_windows.bat
+++ etc/run_test_for_windows.bat
@@ -1,6 +1,6 @@
 chcp 65001
-echo тест
+echo программа datana_siemens_v1
 pause
 set JAVA_HOME=d:\apps\jdk8_131\jre
 set PATH=$JAVA_HOME$\bin;%PATH%
-java -jar Siemens-K4-1.0-SNAPSHOT-jar-with-dependencies.jar -Dapp.dir="d:/apps/datana_siemens_v1"
+java -Dapp.dir="d:/apps/datana_siemens_v1" -jar Siemens-K4-1.0-SNAPSHOT-jar-with-dependencies.jar

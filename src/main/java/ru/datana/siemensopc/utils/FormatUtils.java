eb016c87eb8c423b0d64a06092bff8f9da407f93
Konstantin Kakunin (lanit) <kakunin@lanit.ru>
2020-03-07 12:54:59 +0300

    оформление под релиз v12

--- src/main/java/ru/datana/siemensopc/utils/FormatUtils.java
+++ src/main/java/ru/datana/siemensopc/utils/FormatUtils.java
@@ -1,6 +1,5 @@
 package ru.datana.siemensopc.utils;
 
-import Moka7.S7;
 import lombok.extern.slf4j.Slf4j;
 import ru.datana.siemensopc.config.EnumFormatBytesType;
 
@@ -18,7 +17,6 @@ public class FormatUtils {
     public static void formatBytes(String methodName, byte[] buffer, EnumFormatBytesType typeFormat) throws AppException {
         String prefixLog = "[DUMP] [Источник:" + methodName + "] ";
 
-        log.info(prefixLog + " ============== Начало блока ==============");
         if (buffer == null || buffer.length == 0) {
             log.warn(prefixLog + "<ПУСТО>");
             return;
@@ -27,43 +25,16 @@ public class FormatUtils {
 
         if (typeFormat == EnumFormatBytesType.CLASSIC) {
             log.info(prefixLog + " десятичные числа по байтам: " + Arrays.toString(buffer));
+
         } else if (typeFormat == EnumFormatBytesType.BIG_INTEGER) {
-            log.info(prefixLog + " как большое целое число: " + new BigInteger(buffer));
+            log.info(prefixLog + " как большое целое число: " + new BigInteger(buffer).toString());
+
         } else if (typeFormat == EnumFormatBytesType.HEX) {
-            log.info(prefixLog + " как большое целое число: " + new BigInteger(buffer));
-            log.info(prefixLog + " как hex по байтам: ");
-            int r = 0;
-            StringBuilder hexStringBuffer = new StringBuilder();
+            log.info(prefixLog + " как hex по байтам: " + new BigInteger(buffer).toString(16));
 
-            for (int i = 0; i < buffer.length; i++) {
-                int v = (buffer[i] & 0x0FF);
-                String hv = Integer.toHexString(v);
-
-                if (hv.length() == 1)
-                    hv = "0" + hv + " ";
-                else
-                    hv = hv + " ";
-
-                hexStringBuffer.append(hv);
-
-                r++;
-                if (r == 16) {
-                    log.info(prefixLog + hexStringBuffer + " " + S7.GetPrintableStringAt(buffer, i - 15, 16));
-                    hexStringBuffer = new StringBuilder();
-                    r = 0;
-                }
-            }
-            int L = hexStringBuffer.length();
-            if (L > 0) {
-                while (hexStringBuffer.length() < 49)
-                    hexStringBuffer.append(" ");
-                log.info(prefixLog + hexStringBuffer + S7.GetPrintableStringAt(buffer, buffer.length - r, r));
-            } else
-                log.info(prefixLog + "<ПУСТО>");
         } else {
             String args = "methodName = " + methodName + "byte[] = " + Arrays.toString(buffer) + ", typeFormat = " + typeFormat;
             throw new AppException(TypeException.INVALID_USER_INPUT_DATA, "не понятен формат вывода", args, null);
         }
-        log.info(prefixLog + " ============== Конец блока ==============");
     }
 }

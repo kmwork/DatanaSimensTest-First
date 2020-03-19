9215def4ba454d7d7ffd2c86de477b884925a7f8
Konstantin Kakunin <kmwork@yandex.ru>
2020-03-19 03:36:21 +0300

    fix

--- src/main/java/ru/datana/siemensopc/config/AppConts.java
+++ src/main/java/ru/datana/siemensopc/config/AppConts.java
@@ -10,7 +10,7 @@ public class AppConts {
     public static final String ENCODING = "UTF8";
 
 
-    public static final String SUCCESS_LOG_PREFIX = "[App-Ошибка] ";
-    public static final String ERROR_LOG_PREFIX = "[App-Успешно] ";
+    public static final String SUCCESS_LOG_PREFIX = "[App-Успешно] ";
+    public static final String ERROR_LOG_PREFIX = "[App-Ошибка] ";
     public static final String APP_LOG_PREFIX = "[App-Danata] ";
 }
